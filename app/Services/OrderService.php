9fc97e56c543d3528b48f8ba6e7d69d74c469c1b
“[firstname <alikehtari.ak@gmail.com>
2025-03-03 14:03:44 +0330

    asd3

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -86,6 +86,7 @@ class OrderService
             return; // Do nothing if the link already exists
         }
         $channelId = $order->product?->channel?->channel_id;
+        dd($channelId);
         $link = Respond::create_invite_link($channelId);
         if ($link) {
             $order->update(['link' => $link]);
