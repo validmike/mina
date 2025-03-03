4a4bdd2f8f39d99f679c4b5223c0e7a8999ab4c8
“[firstname <alikehtari.ak@gmail.com>
2025-03-03 13:28:18 +0330

    banned page

---
+++ resources/js/Pages/Banned.jsx
@@ -0,0 +1,12 @@
+export default function Banned() {
+    return (
+      <div className="h-screen flex items-center justify-center bg-red-900 text-white">
+        <h1 className="text-3xl font-bold text-center max-w-md">
+          You have been permanently banned for violating the rules.  
+          Your actions were so disgraceful that you're no longer welcome here.  
+          Find another place to ruin.  
+        </h1>
+      </div>
+    );
+  }
+  
