b59d153ae9cb4be5c593aa7f387b36a030ec1604
“[firstname <alikehtari.ak@gmail.com>
2025-04-21 14:47:51 +0330

    dasd

--- resources/js/Pages/Demo.jsx
+++ resources/js/Pages/Demo.jsx
@@ -82,7 +82,7 @@ export default function Demo({ demos, userLevel, watermark }) {
                             href="/demos?product_id=5"
                             as='button'
                             className={`px-4 py-2 text-sm font-medium ${
-                                productId === '2'
+                                productId === '5'
                                     ? 'text-blue-600 border-b-2 border-blue-600'
                                     : 'text-gray-500 hover:text-gray-700'
                             }`}
