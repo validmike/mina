bcfb0e0e131fa7877df2ff5fae2dd77876759849
“[firstname <alikehtari.ak@gmail.com>
2025-05-11 11:53:59 +0330

    proof only for some

--- resources/js/Pages/Proofs.jsx
+++ resources/js/Pages/Proofs.jsx
@@ -1,89 +1,93 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, Link } from '@inertiajs/react';
-import ScreenshotSlider from '@/Components/ScreenshotSlider';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head, Link } from "@inertiajs/react";
+import ScreenshotSlider from "@/Components/ScreenshotSlider";
 
-export default function Proofs({watermark , country}) {
-
+export default function Proofs({ watermark, country }) {
     const sliders = [
         {
             images: [
                 "https://files.catbox.moe/v0d21e.jpg",
-                "https://files.catbox.moe/7pcjok.jpg"
+                "https://files.catbox.moe/7pcjok.jpg",
             ],
-            watermark: watermark
+            watermark: watermark,
         },
         {
             images: [
                 "https://files.catbox.moe/126puh.jpg",
-                "https://files.catbox.moe/7wd8bu.jpg"
+                "https://files.catbox.moe/7wd8bu.jpg",
             ],
-            watermark: watermark
+            watermark: watermark,
         },
         {
             images: [
                 "https://files.catbox.moe/lnuscn.jpg",
-                "https://files.catbox.moe/zgji6l.jpg"
+                "https://files.catbox.moe/zgji6l.jpg",
             ],
-            watermark: watermark
+            watermark: watermark,
         },
 
         {
             images: [
                 "https://files.catbox.moe/8uu6jw.jpg",
-                "https://files.catbox.moe/lnila1.jpg"
+                "https://files.catbox.moe/lnila1.jpg",
             ],
-            watermark: watermark
+            watermark: watermark,
         },
         {
             images: [
                 "https://files.catbox.moe/c6utw5.jpg",
-                "https://files.catbox.moe/6b51il.jpg"
+                "https://files.catbox.moe/6b51il.jpg",
             ],
-            watermark: watermark
+            watermark: watermark,
         },
         {
             images: [
                 "https://files.catbox.moe/kxthgl.jpg",
-                "https://files.catbox.moe/s5wyhp.jpg"
+                "https://files.catbox.moe/s5wyhp.jpg",
             ],
-            watermark: watermark
+            watermark: watermark,
         },
         {
             images: [
                 "https://files.catbox.moe/eivs8x.jpg",
-                "https://files.catbox.moe/ed5980.jpg"
+                "https://files.catbox.moe/ed5980.jpg",
             ],
-            watermark: watermark
+            watermark: watermark,
         },
         {
             images: [
                 "https://files.catbox.moe/6wvnqy.jpg",
-                "https://files.catbox.moe/72zbfj.jpg"
+                "https://files.catbox.moe/72zbfj.jpg",
             ],
-            watermark: watermark
+            watermark: watermark,
         },
         {
             images: [
                 "https://files.catbox.moe/826rt1.jpg",
-                "https://files.catbox.moe/wd8akc.jpg"
+                "https://files.catbox.moe/wd8akc.jpg",
             ],
-            watermark: watermark
+            watermark: watermark,
         },
     ];
 
     return (
         <AuthenticatedLayout
-            header={<h2 className="text-xl font-semibold leading-tight text-gray-800">Proofs</h2>}
+            header={
+                <h2 className="text-xl font-semibold leading-tight text-gray-800">
+                    Proofs
+                </h2>
+            }
         >
             <Head title="Proofs" />
 
             <div className="py-12">
                 <div className="mb-4">
                     <p>
-                        these are the people who bought the VIP group use a VPN if not loaded:
+                        these are the people who bought the VIP group use a VPN
+                        if not loaded:
                     </p>
                 </div>
-                {country !== 'BR' && (
+                {/* {country !== 'BR' && (
                     <div className="mb-4 p-4 border-l-4 border-yellow-500 bg-yellow-100 text-yellow-800">
                         <p className="font-bold">Do you need 100% proof that this is not a scam?</p>
                         <p>
@@ -91,11 +95,35 @@ export default function Proofs({watermark , country}) {
                             <Link as='button' href="/guide/guarantee" className="text-blue-600 hover:underline"> Click here for more info</Link>.
                         </p>
                     </div>
+                )} */}
+                {["US", "UK", "CA", "AU", "DE", "FR", "ES"].includes(
+                    country
+                ) && (
+                    <div className="mb-4 p-4 border-l-4 border-yellow-500 bg-yellow-100 text-yellow-800">
+                        <p className="font-bold">
+                            Do you need 100% proof that this is not a scam?
+                        </p>
+                        <p>
+                            If these screenshots are not enough, I have a way to
+                            make you sure this is not a scam.
+                            <Link
+                                as="button"
+                                href="/guide/guarantee"
+                                className="text-blue-600 hover:underline"
+                            >
+                                {" "}
+                                Click here for more info
+                            </Link>
+                            .
+                        </p>
+                    </div>
                 )}
 
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg p-6">
-                        <h3 className="text-lg font-semibold mb-4">Proof Screenshots</h3>
+                        <h3 className="text-lg font-semibold mb-4">
+                            Proof Screenshots
+                        </h3>
                         <div className="space-y-8">
                             {sliders.map((slider, index) => (
                                 <ScreenshotSlider
