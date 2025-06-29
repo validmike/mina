324b16eba341d77585a44d7ab8f2c7cb277409dd
“[firstname <alikehtari.ak@gmail.com>
2025-06-29 10:36:07 +0330

    minor ui

--- resources/js/Components/DemoCard.jsx
+++ resources/js/Components/DemoCard.jsx
@@ -37,7 +37,7 @@ const DemoCard = ({ url, watermark , level }) => {
 
             {/* Watermark - Shown When Revealed */}
             {revealed && (
-                <div className="absolute inset-0 flex items-center justify-center text-white font-bold text-sm bg-black/20 z-10">
+                <div className="absolute inset-0 flex items-center justify-center text-white font-normal  text-xs bg-black/20 z-10">
                     {watermark}
                 </div>
             )}
