2096175ead67be38ec0b1be703d288d56b54ae9f
“[firstname <alikehtari.ak@gmail.com>
2026-05-14 15:40:27 +0330

    redirect disabled

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,64 +1,10 @@
-import { useEffect, useState } from 'react';
-import { FaSpinner } from 'react-icons/fa';
+import { useEffect } from 'react';
 
 export default function Guest({ children }) {
-    const [isChecking, setIsChecking] = useState(true);
-
     useEffect(() => {
-        let cancelled = false;
-
-        const verify = () => {
-            const tg = window.Telegram?.WebApp;
-            if (!tg) {
-                window.location.href = 'https://telegram.org/';
-                return;
-            }
-
-            try { tg.ready(); } catch (_) {}
-            try { tg.expand(); } catch (_) {}
-
-            if (!tg.initDataUnsafe?.user) {
-                window.location.href = 'https://telegram.org/';
-                return;
-            }
-
-            if (!cancelled) setIsChecking(false);
-        };
-
-        if (window.Telegram?.WebApp) {
-            verify();
-            return;
-        }
-
-        const interval = setInterval(() => {
-            if (window.Telegram?.WebApp) {
-                clearInterval(interval);
-                clearTimeout(timeout);
-                verify();
-            }
-        }, 50);
-
-        const timeout = setTimeout(() => {
-            clearInterval(interval);
-            if (!window.Telegram?.WebApp) {
-                window.location.href = 'https://telegram.org/';
-            }
-        }, 3000);
-
-        return () => {
-            cancelled = true;
-            clearInterval(interval);
-            clearTimeout(timeout);
-        };
+        try { window.Telegram?.WebApp?.ready(); } catch (_) {}
+        try { window.Telegram?.WebApp?.expand(); } catch (_) {}
     }, []);
-
-    if (isChecking) {
-        return (
-            <div className="flex min-h-screen items-center justify-center bg-gray-100">
-                <FaSpinner className="animate-spin text-gray-600 text-4xl" />
-            </div>
-        );
-    }
 
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
